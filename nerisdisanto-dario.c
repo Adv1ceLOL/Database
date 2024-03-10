e0746eb16c56f359ceab0ca74c0b8caf2b8e5e44
AdviceLOL <dationeris@gmail.com>
2024-03-10 18:36:20 +0100

    esercizio

--- nerisdisanto-dario.c
+++ nerisdisanto-dario.c
@@ -42,6 +42,7 @@ IndexNodeString* treeGenString(char* root_value) {
         return NULL;
     } 
     root->value = root_value;
+    //printf("Riga 45: %s\n", root->value);
     root->left = NULL;
     root->right = NULL;
     return root;
@@ -60,84 +61,92 @@ IndexNodeInt* treeGenInt(int root_value) {
 }
 
 
-void insertName(IndexNodeString* node, struct Persona * persona){
-    if(persona->name <= node->value){
-        if(node->left == NULL){
+void insertName(IndexNodeString* root, struct Persona * persona){
+    //printf("Riga 65: %s\n",persona->name);  //controllo
+    if(root == NULL)return;
+    if(persona->name <= root->value){
+        if(root->left == NULL){
             IndexNodeString* node = treeGenString(persona->name);
-            node->left = node;
+            //printf("Riga 69: %s\n", node->value);
+            root->left = node;
             return;
         }
-        insertName(node->left, persona);
+        insertName(root->left, persona);
         return;
     }   
-    if(node->right == NULL){
+    if(root->right == NULL){
         IndexNodeString* node = treeGenString(persona->name);
-        node->right = node;
+        //printf("Riga 78: %s\n", node->value);
+        root->right = node;
         return;
     }
-    insertName(node->right, persona);
+    insertName(root->right, persona);
 }
 
-void insertSurname(IndexNodeString* node, struct Persona * persona){
-    if(persona->surname <= node->value){
-        if(node->left == NULL){
+void insertSurname(IndexNodeString* root, struct Persona * persona){
+    if(root == NULL)return;
+    if(persona->surname <= root->value){
+        if(root->left == NULL){
             IndexNodeString* node = treeGenString(persona->surname);
-            node->left = node;
+            root->left = node;
             return;
         }
-        insertSurname(node->left, persona);
+        insertSurname(root->left, persona);
         return;
     }   
-    if(node->right == NULL){
+    if(root->right == NULL){
         IndexNodeString* node = treeGenString(persona->surname);
-        node->right = node;
+        root->right = node;
         return;
     }
-    insertSurname(node->right, persona);
+    insertSurname(root->right, persona);
 }
 
-void insertAddress(IndexNodeString* node, struct Persona * persona){
-    if(persona->address <= node->value){
-        if(node->left == NULL){
+void insertAddress(IndexNodeString* root, struct Persona * persona){
+    if(root == NULL)return;
+    if(persona->address <= root->value){
+        if(root->left == NULL){
             IndexNodeString* node = treeGenString(persona->address);
-            node->left = node;
+            root->left = node;
             return;
         }
-        insertAddress(node->left, persona);
+        insertAddress(root->left, persona);
         return;
     }   
-    if(node->right == NULL){
+    if(root->right == NULL){
         IndexNodeString* node = treeGenString(persona->address);
-        node->right = node;
+        root->right = node;
         return;
     }
-    insertAddress(node->right, persona);
+    insertAddress(root->right, persona);
 }
 
-void insertAge(IndexNodeInt* node, struct Persona * persona){
-    if(persona->age <= node->value){
-        if(node->left == NULL){
+void insertAge(IndexNodeInt* root, struct Persona * persona){
+    if(root == NULL)return;
+    if(root->value <= persona->age){
+        if(root->left == NULL){
             IndexNodeInt* node = treeGenInt(persona->age);
-            node->left = node;
+            root->left = node;
             return;
         }
-        insertAge(node->left, persona);
+        insertAge(root->left, persona);
         return;
     }
-    if(node->right == NULL){
+    if(root->right == NULL){
         IndexNodeInt* node = treeGenInt(persona->age);
-        node->right = node;
+        root->right = node;
         return;
     }
-    insertAge(node->right, persona);
+    insertAge(root->right, persona);
 }
 
 void insert(Database * database, struct Persona * persona){
-    if(database == NULL) return;
+    if(persona == NULL) return;
     //* Creo un insert per ogni oggetto in Persona
 
     //! NAME INSERTION
     insertName(database->name, persona);
+    //printf("Riga 143: %s\n", database->name);
     //! SURNAME INSERTION
     insertSurname(database->surname, persona);
     //! ADDRESS INSERTION
@@ -152,11 +161,11 @@ struct Persona* findByNameAux(IndexNodeString* node, char* name){
     if(strcmp(node->value,name) == 0) return (struct Persona*) node;
     if(node->left != NULL)findByNameAux(node->left, name);
     if(node->right != NULL)findByNameAux(node->right, name);
-    
+
 }
 
 struct Persona* findByName(Database * database, char * name){
-    if(database == NULL)return NULL; 
+    if(strcmp(name,"") == 0)return NULL; 
     findByNameAux(database->name, name);
 }
 
@@ -168,7 +177,7 @@ struct Persona* findBySurnameAux(IndexNodeString* node, char* surname){
 }
 
 struct Persona* findBySurname(Database * database, char * surname){
-    if(database == NULL)return NULL; 
+    if(strcmp(surname,"") == 0)return NULL; 
     findBySurnameAux(database->surname, surname);
 }
 
@@ -180,7 +189,7 @@ struct Persona* findByAdressAux(IndexNodeString* node, char* address){
 }
 
 struct Persona* findByAddress(Database * database, char * address){
-    if(database == NULL)return NULL; 
+    if(strcmp(address,"") == 0)return NULL; 
     findByAdressAux(database->address, address);
 }
 
@@ -192,12 +201,108 @@ struct Persona* findByAgeAux(IndexNodeInt* node, int age){
 }
 
 struct Persona* findByAge(Database * database, int age){
-    if(database == NULL)return NULL;
+    if(age == 0)return NULL;
     findByAgeAux(database->age, age);
 }
 
-/*
-void free_all(Database* database){
-    free(database);
+void printTreeString(IndexNodeString* node, int space) {
+    if (node == NULL)
+        return;
+
+    space += 10;
+
+    printTreeString(node->right, space);
+
+    printf("\n");
+    for (int i = 10; i < space; i++)
+        printf(" ");
+    printf("%s\n", node->value);
+
+    printTreeString(node->left, space);
 }
-*/
+
+void printTreeInt(IndexNodeInt* node, int space) {
+    if (node == NULL)
+        return;
+
+    space += 10;
+
+    printTreeInt(node->right, space);
+
+    printf("\n");
+    for (int i = 10; i < space; i++)
+        printf(" ");
+    printf("%d\n", node->value);
+
+    printTreeInt(node->left, space);
+}
+
+void printPersona(struct Persona* person) {
+        printf("Name: %s\n", person->name);
+        printf("Surname: %s\n", person->surname);
+        printf("Address: %s\n", person->address);
+        printf("Age: %d\n", person->age);
+}
+
+void freeDatabase(Database* root){
+    free(root);
+}
+
+int main(int argc, const char * argv[]) {
+    Database* database = malloc(sizeof(Database));
+    // Creazione dei roots:
+    database->name = treeGenString("Nomi");
+    database->surname = treeGenString("Cognomi");
+    database->address = treeGenString("Indirizzi");
+    database->age = treeGenInt(0);
+
+    struct Persona * deadpool = malloc(sizeof(struct Persona));
+    char* name = "Ryan";
+    char* surname = "Reynolds";
+    char* address = "Los Angeles";
+    int age = 47;
+
+    strcpy(deadpool->name, name);
+    strcpy(deadpool->surname, surname);
+    strcpy(deadpool->address, address);
+    deadpool->age = age;
+
+    struct Persona * homelander = malloc(sizeof(struct Persona));
+    name = "Antony";
+    surname = "Starr";
+    address = "New York";
+    age = 48;
+
+    strcpy(homelander->name, name);
+    strcpy(homelander->surname, surname);
+    strcpy(homelander->address, address);
+    homelander->age = age;
+
+    struct Persona * LastWeekTonight = malloc(sizeof(struct Persona));
+    name = "John";
+    surname = "Oliver";
+    address = "Liverpool";
+    age = 46;
+
+    strcpy(LastWeekTonight->name, name);
+    strcpy(LastWeekTonight->surname, surname);
+    strcpy(LastWeekTonight->address, address);
+    LastWeekTonight->age = age;
+
+    insert(database, deadpool);
+    insert(database, homelander);
+    insert(database, LastWeekTonight);
+
+    
+    //struct Persona* find = findByName(database, "Ryan");
+    //printPersona(find);
+
+    printTreeString(database->name,0);
+    printTreeString(database->surname,0);
+    printTreeString(database->address,0);
+    printTreeInt(database->age,0);
+
+    freeDatabase(database);
+}
+
+
